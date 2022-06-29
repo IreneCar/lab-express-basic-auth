04941d681b2d9d4a3a61aeb327dcb18040e4111d
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-29 19:09:20 +0200

    iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -36,6 +36,43 @@ router.post('/signup', (req, res, next) => {
     })
     .then(userFromDB => {
       console.log('Newly created user is: ', userFromDB);
+      res.redirect('/userProfile');
+    })
+
+    .catch(error => next(error));
+
+});
+
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+
+//////////// L O G I N ///////////
+ 
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
     })
     .catch(error => next(error));
 });
