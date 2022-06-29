66dd712854e9e8336e43711c860e711bb8a17a30
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-29 17:35:51 +0200

    User registration

---
+++ routes/auth.routes.js
@@ -0,0 +1,43 @@
+// routes/auth.routes.js
+const { Router } = require('express');
+const router = new Router();
+
+// other imports
+const User = require('../models/User.model');
+
+// the setup code skipped
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+
+// GET route ==> to display the signup form to users
+router.get('/signup', (req, res) => res.render('auth/signup'));
+
+
+// POST route ==> to process form data
+router.post('/signup', (req, res, next) => {
+  // console.log("The form data: ", req.body);
+ 
+  const { username, email, password } = req.body;
+ 
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        // username: username
+        username,
+        email,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+    })
+    .catch(error => next(error));
+});
+
+module.exports = router;
